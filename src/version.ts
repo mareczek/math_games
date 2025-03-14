ea03842bb81a77304b0e50c1ae8d5ca6ce5b22b7
Marek Pietrucha <marek@enginearch.com>
2025-03-14 12:51:28 +0100

    Update version to 1.0.2

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // This file contains the current version of the application
 // Update this version number when deploying a new version
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.0.2';
